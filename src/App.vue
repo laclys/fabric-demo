04ae15f4e75c2761b5270fbb4ea0bbdcce9ca4e4
Lac <laclys@126.com>
2023-10-09 00:52:54 +0800

    缩放画布

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <canvas
+      id="canvas"
       width="400"
       height="400"
-      id="canvas"
       style="border: 1px solid #ccc"
     ></canvas>
   </div>
@@ -13,25 +13,40 @@
 import { onMounted } from "vue";
 import { fabric } from "fabric";
 
-// 初始化
 function init() {
-  const canvas = new fabric.Canvas("canvas", {
-    isDrawingMode: true, // 开启绘图模式
+  // 初始化画布
+  const canvas = new fabric.Canvas("canvas");
+
+  // 矩形（参照物）
+  const rect = new fabric.Rect({
+    top: 10,
+    left: 10,
+    width: 40,
+    height: 40,
+    fill: "orange",
   });
 
-  // 设置画笔颜色
-  canvas.freeDrawingBrush.color = "#11999e";
+  // 圆形（参照物）
+  const circle = new fabric.Circle({
+    top: 30,
+    left: 30,
+    radius: 50,
+    fill: "green",
+  });
+  canvas.add(rect, circle); // 将矩形和圆形添加到画布中
 
-  // 设置画笔粗细
-  canvas.freeDrawingBrush.width = 10;
+  // 监听鼠标滚轮事件
+  canvas.on("mouse:wheel", (opt) => {
+    let delta = opt.e.deltaY; // 滚轮向上滚一下是 -100，向下滚一下是 100
+    let zoom = canvas.getZoom(); // 获取画布当前缩放值
 
-  // 画笔投影
-  canvas.freeDrawingBrush.shadow = new fabric.Shadow({
-    blur: 10,
-    offsetX: 10,
-    offsetY: 10,
-    affectStroke: true,
-    color: "#30e3ca",
+    // 控制缩放范围在 0.01~20 的区间内
+    zoom *= 0.999 ** delta;
+    if (zoom > 20) zoom = 20;
+    if (zoom < 0.01) zoom = 0.01;
+
+    // 设置画布缩放比例
+    canvas.setZoom(zoom);
   });
 }
 
