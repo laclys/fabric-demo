1c69d216b38009b1e234cb0e6ab1f299b623793b
Lac <laclys@126.com>
2023-10-08 23:11:49 +0800

    使用 fabric 接管容器，并画一个矩形

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from "vue";
+import { fabric } from "fabric"; // 引入 fabric
+
+function init() {
+  const canvas = new fabric.Canvas("c"); // 这里传入的是canvas的id
+
+  // 创建一个长方形
+  const rect = new fabric.Rect({
+    top: 30, // 距离容器顶部 30px
+    left: 30, // 距离容器左侧 30px
+    width: 100, // 宽 100px
+    height: 60, // 高 60px
+    fill: "red", // 填充 红色
+  });
+
+  // 在canvas画布中加入矩形（rect）。add是“添加”的意思
+  canvas.add(rect);
+}
+
+// 需要在页面容器加载完才能开始初始化（页面加载完才找到 canvas 元素）
+// onMounted 是 Vue3 提供的一个页面生命周期函数：实例被挂载后调用。
+// onMounted 官方文档说明：https://v3.cn.vuejs.org/guide/composition-api-lifecycle-hooks.html
+onMounted(() => {
+  init(); // 执行初始化函数
+});
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <canvas
+      width="400"
+      height="400"
+      id="c"
+      style="border: 1px solid #ccc"
+    ></canvas>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
