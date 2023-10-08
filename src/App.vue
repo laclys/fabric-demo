c9e8b0fb63842a0746a35f3f07bdc3241421bba9
Lac <laclys@126.com>
2023-10-09 00:08:47 +0800

    背景图片处理

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <script setup>
 import { onMounted } from "vue";
-import { fabric } from "fabric"; // 引入 fabric
+import { fabric } from "fabric";
 
 function init() {
-  const canvas = new fabric.Canvas("canvas", {
-    width: 300, // 画布宽度
-    height: 300, // 画布高度
-    backgroundColor: "#eee", // 画布背景色
-  });
+  const canvas = new fabric.Canvas("canvas");
 
-  // 圆形
-  const circle = new fabric.Circle({
-    radius: 30, // 圆的半径
-    top: 20, // 距离容器顶部 20px
-    left: 20, // 距离容器左侧 20px
-    fill: "pink", // 填充 粉色
+  // // 设置背景图
+  // // 参数1：背景图资源（可以引入本地，也可以使用网络图）
+  // // 参数2：设置完背景图执行以下重新渲染canvas的操作，这样背景图就会展示出来了
+  // canvas.setBackgroundImage("/src/assets/pic.jpg", canvas.renderAll.bind(canvas), {
+  //   // angle: 15, // 旋转背景图
+  // });
+  fabric.Image.fromURL("/src/assets/pic.jpg", (img) => {
+    // 设置背景图
+    canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
+      scaleX: canvas.width / img.width, // 计算出图片要拉伸的宽度
+      scaleY: canvas.height / img.height, // 计算出图片要拉伸的高度
+    });
   });
-
-  canvas.add(circle); // 将圆形添加到 canvas 画布里
 }
+// 使用了 fabric.Image.fromURL 这个 api 来加载图片，第一个参数是图片地址，第二个参数是回调函数。
+
 
 onMounted(() => {
   init();
