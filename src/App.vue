cb3c4f250493c0c2521c38a78b7ad2dd19fe3a07
Lac <laclys@126.com>
2023-10-09 00:27:09 +0800

    文本框

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,11 @@ import jailCellBars from "./assets/jail_cell_bars.png"; // 引入背景图
 
 function init() {
   const canvas = new fabric.Canvas("canvas");
-  const text = new fabric.IText("Bilibili");
-  canvas.add(text);
+  const textbox = new fabric.Textbox("this is textbox", {
+    width: 250,
+  });
+  canvas.add(textbox);
 }
-/* 使用 new fabric.IText 可以创建可编辑文本，用法和 new fabric.Text 一样。
-
-IText 比 Text 多了个大写 “I” 在首字母上。 */
 
 onMounted(() => {
   init();
