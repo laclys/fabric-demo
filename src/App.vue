d85251877826152f75aa93ffccd58c574b21a90d
Lac <laclys@126.com>
2023-10-09 00:24:15 +0800

    可编辑文本

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,12 @@ import jailCellBars from "./assets/jail_cell_bars.png"; // 引入背景图
 
 function init() {
   const canvas = new fabric.Canvas("canvas");
-  const text = new fabric.Text("Bilibili");
+  const text = new fabric.IText("Bilibili");
   canvas.add(text);
 }
+/* 使用 new fabric.IText 可以创建可编辑文本，用法和 new fabric.Text 一样。
+
+IText 比 Text 多了个大写 “I” 在首字母上。 */
 
 onMounted(() => {
   init();
