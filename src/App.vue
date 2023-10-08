584b973b8e0a45ff0332dbc81ff491931a1de6f6
Lac <laclys@126.com>
2023-10-09 00:49:25 +0800

    点击事件

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
       id="canvas"
       style="border: 1px solid #ccc"
     ></canvas>
+    <button @click="addClickEvent">添加画布点击事件</button>
+    <button @click="removeClickEvent">移除画布点击事件</button>
   </div>
 </template>
 
@@ -13,31 +15,42 @@
 import { onMounted } from "vue";
 import { fabric } from "fabric";
 
-// 初始化
+let canvas = null;
+
+// 初始化画布
 function init() {
-  const canvas = new fabric.Canvas("canvas");
+  canvas = new fabric.Canvas("canvas");
 
   const rect = new fabric.Rect({
-    left: 100,
-    top: 100,
+    top: 20,
+    left: 20,
     width: 100,
-    height: 100,
-    fill: "red",
+    height: 50,
+    fill: "#9896f1",
   });
 
-  // 设置矩形动画
-  /*   第一个参数是要设置动画的属性。
-
-第二个参数是动画的结束值。
-
-第三个参数是一个对象 */
-  rect.animate("angle", "+=360", { // "+=360"相对动画 
-    onChange: canvas.renderAll.bind(canvas), // 每次刷新的时候都会执行
-    duration: 2000, // ms 默认500ms
-    easing: fabric.util.ease.easeOutBounce, // 缓冲效果
+  // 给矩形添加一个选中事件
+  rect.on("selected", (options) => {
+    console.log("选中矩形啦", options);
   });
-
   canvas.add(rect);
+
+  addClickEvent();
+}
+
+// 移除画布点击事件
+function removeClickEvent() {
+  canvas.off("mouse:down");
+}
+
+// 添加画布点击事件
+function addClickEvent() {
+  removeClickEvent(); // 在添加事件之前先把该事件清除掉，以免重复添加
+  canvas.on("mouse:down", (options) => {
+    console.log(
+      `x轴坐标: ${options.e.clientX};    y轴坐标: ${options.e.clientY}`
+    );
+  });
 }
 
 onMounted(() => {
