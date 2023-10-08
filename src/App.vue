86d5c050267220adb83cba0496000e41f8f0f388
Lac <laclys@126.com>
2023-10-09 00:53:28 +0800

    元素禁止选中

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
-  <div>
-    <canvas
-      id="canvas"
-      width="400"
-      height="400"
-      style="border: 1px solid #ccc"
-    ></canvas>
-  </div>
+  <canvas
+    width="400"
+    height="400"
+    id="canvas"
+    style="border: 1px solid #ccc"
+  ></canvas>
 </template>
 
 <script setup>
@@ -14,40 +12,20 @@ import { onMounted } from "vue";
 import { fabric } from "fabric";
 
 function init() {
-  // 初始化画布
   const canvas = new fabric.Canvas("canvas");
 
-  // 矩形（参照物）
   const rect = new fabric.Rect({
-    top: 10,
-    left: 10,
-    width: 40,
-    height: 40,
-    fill: "orange",
+    top: 100,
+    left: 100,
+    width: 200,
+    height: 100,
+    fill: "red",
   });
 
-  // 圆形（参照物）
-  const circle = new fabric.Circle({
-    top: 30,
-    left: 30,
-    radius: 50,
-    fill: "green",
-  });
-  canvas.add(rect, circle); // 将矩形和圆形添加到画布中
+  // 元素禁止选中
+  rect.selectable = false;
 
-  // 监听鼠标滚轮事件
-  canvas.on("mouse:wheel", (opt) => {
-    let delta = opt.e.deltaY; // 滚轮向上滚一下是 -100，向下滚一下是 100
-    let zoom = canvas.getZoom(); // 获取画布当前缩放值
-
-    // 控制缩放范围在 0.01~20 的区间内
-    zoom *= 0.999 ** delta;
-    if (zoom > 20) zoom = 20;
-    if (zoom < 0.01) zoom = 0.01;
-
-    // 设置画布缩放比例
-    canvas.setZoom(zoom);
-  });
+  canvas.add(rect);
 }
 
 onMounted(() => {
