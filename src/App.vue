357288bfbf749488fc357977af55881cfae39c1d
Lac <laclys@126.com>
2023-10-09 00:34:11 +0800

    渐变

--- src/App.vue
+++ src/App.vue
@@ -5,31 +5,47 @@ import jailCellBars from "./assets/jail_cell_bars.png"; // 引入背景图
 
 function init() {
   const canvas = new fabric.Canvas("canvas");
-  const text = new fabric.Text("托马斯", {
-    top: 40,
-    left: 40,
-    fontSize: 120,
-    backgroundColor: "green", // 背景色：绿色
-    fill: "orange", // 填充色：橙色
-    stroke: "#f6416c", // 边框颜色：粉色
-    strokeWidth: 3, // 边框粗细：3px
-    strokeDashArray: [20, 5, 14], // 边框虚线规则：填充20px 空5px 填充14px 空20px 填充5px ……
-    shadow: "10px 20px 6px rgba(10, 20, 30, 0.4)", // 投影：向右偏移10px，向下偏移20px，羽化6px，投影颜色及透明度
-    transparentCorners: false, // 选中时，角是被填充了。true 空心；false 实心
-    borderColor: "#16f1fc", // 选中时，边框颜色：天蓝
-    borderScaleFactor: 5, // 选中时，边的粗细：5px
-    borderDashArray: [20, 5, 10, 7], // 选中时，虚线边的规则
-    cornerColor: "#a1de93", // 选中时，角的颜色是 青色
-    cornerStrokeColor: "pink", // 选中时，角的边框的颜色是 粉色
-    cornerStyle: "circle", // 选中时，叫的属性。默认rect 矩形；circle 圆形
-    cornerSize: 20, // 选中时，角的大小为20
-    cornerDashArray: [10, 2, 6], // 选中时，虚线角的规则
-    selectionBackgroundColor: "#7f1300", // 选中时，选框的背景色：朱红
-    padding: 40, // 选中时，选择框离元素的内边距：40px
-    borderOpacityWhenMoving: 0.6, // 当对象活动和移动时，对象控制边界的不透明度
+  // 圆
+  let circle = new fabric.Circle({
+    left: 100,
+    top: 100,
+    radius: 50,
   });
 
-  canvas.add(text);
+  /*   // 线性渐变
+  let gradient = new fabric.Gradient({
+    type: "linear", // linear or radial
+    gradientUnits: "pixels", // pixels or pencentage 像素 或者 百分比
+    coords: { x1: 0, y1: 0, x2: circle.width, y2: 0 }, // 至少2个坐标对（x1，y1和x2，y2）将定义渐变在对象上的扩展方式
+    colorStops: [
+      // 定义渐变颜色的数组
+      { offset: 0, color: "red" },
+      { offset: 0.2, color: "orange" },
+      { offset: 0.4, color: "yellow" },
+      { offset: 0.6, color: "green" },
+      { offset: 0.8, color: "blue" },
+      { offset: 1, color: "purple" },
+    ],
+  }); */
+  /* 径向渐变 */
+  let gradient = new fabric.Gradient({
+    type: "radial",
+    coords: {
+      r1: 50, // 该属性仅径向渐变可用，外圆半径
+      r2: 0, // 该属性仅径向渐变可用，外圆半径
+      x1: 50, // 焦点的x坐标
+      y1: 50, // 焦点的y坐标
+      x2: 50, // 中心点的x坐标
+      y2: 50, // 中心点的y坐标
+    },
+    colorStops: [
+      { offset: 0, color: "#fee140" },
+      { offset: 1, color: "#fa709a" },
+    ],
+  });
+
+  circle.set("fill", gradient);
+  canvas.add(circle);
 }
 
 onMounted(() => {
