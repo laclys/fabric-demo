edbc7cacbfec5d4668c757a5d9731dce27bdc5d3
Lac <laclys@126.com>
2023-10-08 23:14:19 +0800

    可交互画布

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,14 @@ import { fabric } from "fabric"; // 引入 fabric
 function init() {
   const canvas = new fabric.Canvas("c"); // 这里传入的是canvas的id
 
+  // 这个就是可交互的canvas
+
   // 创建一个长方形
   const rect = new fabric.Rect({
-    top: 30, // 距离容器顶部 30px
-    left: 30, // 距离容器左侧 30px
-    width: 100, // 宽 100px
-    height: 60, // 高 60px
+    top: 100, // 距离容器顶部 100px
+    left: 100, // 距离容器左侧 100px
+    width: 30, // 矩形宽度 30px
+    height: 30, // 矩形高度 30px
     fill: "red", // 填充 红色
   });
 
@@ -18,9 +20,6 @@ function init() {
   canvas.add(rect);
 }
 
-// 需要在页面容器加载完才能开始初始化（页面加载完才找到 canvas 元素）
-// onMounted 是 Vue3 提供的一个页面生命周期函数：实例被挂载后调用。
-// onMounted 官方文档说明：https://v3.cn.vuejs.org/guide/composition-api-lifecycle-hooks.html
 onMounted(() => {
   init(); // 执行初始化函数
 });
